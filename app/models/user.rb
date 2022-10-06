fe33a8cd2b6ad7e31d26aecae04772ced3d67c3b
Fernando Mendes <fernando.msantos1@gmail.com>
2022-10-05 22:00:45 -0300

    Add messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password 
+  has_secure_password
+
   validates :username, presence: true, length: { minimum: 5, maximum: 15 }
+  has_many :messages
 end

4ce5d8809f78e462975986b8101307d76e4dbe87
Fernando Mendes <fernando.msantos1@gmail.com>
2022-10-12 12:58:28 -0300

    Add layout login, user session and destroy session

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   
   root 'chatroom#index'
   get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

4daf0e3a01df989fb9a4e89f4b565f01efde96e2
Fernando Mendes <fernando.msantos1@gmail.com>
2022-10-15 20:33:26 -0300

    Add restrictions for resources

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,6 @@
 class SessionController < ApplicationController
+  before_action :loggin_in_redirect, only: %i[new create]
+
   def new
   end
 
@@ -6,18 +8,25 @@ class SessionController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in"
+      flash[:success] = 'You have successfully logged in'
       redirect_to root_path
     else 
-      flash.now[:error] = "There wass something wrong with you login information"
+      flash.now[:error] = 'There wass something wrong with you login information'
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have successfully logged out"
+    flash[:success] = 'You have successfully logged out'
     redirect_to login_path
+  end
+
+  def loggin_in_redirect
+    if logged_in?
+      flash[:error] = 'You are already logged in'
+      redirect_to root_path
+    end
   end
 
 end
